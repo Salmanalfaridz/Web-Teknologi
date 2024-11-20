1e54f2cfbcaedf965e9cd33a09c615a2c144b240
salmanalfaridz <salmanelridzi@gmail.com>
2024-11-20 17:42:01 +0700

    update commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-    return <div>Customer Page</div>;
-  }
+// app/dashboard/customers/page.tsx
+import React from 'react';
+
+const CustomersPage = () => {
+    return <div>Customers</div>;
+};
+
+export default CustomersPage;
